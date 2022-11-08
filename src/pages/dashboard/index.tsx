f7f67d32befd216aa31b30d180c0cea405f2a2a4
MaahiVohra <vohramaahi@gmail.com>
2022-11-08 23:32:53 +0530

    added head to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { useSession, signOut } from "next-auth/react";
+import Head from "next/head";
 
 import { requireAuth } from "../../common/requireAuth";
 
@@ -11,17 +12,25 @@ const Dashboard: NextPage = () => {
   const { data } = useSession();
 
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero bg-base-200 min-h-screen">
+      <Head>
+        <title>Dashboard</title>
+        <meta
+          name="description"
+          content="HealthFirst - A platform for your medical documentation."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="hero-content">
         <div className="max-w-lg">
-          <h1 className="text-5xl text-center font-bold leading-snug text-gray-400">
+          <h1 className="text-center text-5xl font-bold leading-snug text-gray-400">
             You are logged in!
           </h1>
           <p className="my-4 text-center leading-loose">
             You are allowed to visit this page because you have a session,
             otherwise you would be redirected to the login page.
           </p>
-          <div className="my-4 bg-gray-700 rounded-lg p-4">
+          <div className="my-4 rounded-lg bg-gray-700 p-4">
             <pre>
               <code>{JSON.stringify(data, null, 2)}</code>
             </pre>
